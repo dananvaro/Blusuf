776691f1d0a65678a58dfebff4c140da3c0ea2b2
Hajhain Nirmalan <hajankaka12@hotmail.com>
2020-01-29 11:34:13 +0100

    h

--- src/com/company/Klient.java
+++ src/com/company/Klient.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Klient {
+    //initerer
+    private Socket socket;
+    private InputStreamReader innput;
+    private DataOutputStream utput;
+
+    public Klient(String adresse, int portnummer){
+        try {
+            socket = new Socket(adresse, portnummer);
+
+            PrintWriter ut = new PrintWriter(socket.getOutputStream());
+
+            
+            innput = new InputStreamReader(socket.getInputStream());
+            BufferedReader inn = new BufferedReader(innput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
